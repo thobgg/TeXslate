0e568a244426125887f31e7bd20074b63e69698c
thobgg <thomas@bgg-mail.de>
2026-07-08 16:29:32 +0200

    Chore: versionCode 6, versionName 1.0-alpha6 (i18n-Release)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -26,8 +26,8 @@ android {
         applicationId = "de.bgg_home.texdroid"
         minSdk = 26
         targetSdk = 36
-        versionCode = 5
-        versionName = "1.0-alpha5"
+        versionCode = 6
+        versionName = "1.0-alpha6"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
