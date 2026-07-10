989ae22c1429dc5c607f6608a19fa88e593593db
thobgg <thomas@bgg-mail.de>
2026-07-10 16:49:25 +0200

    Release: 1.0-alpha7 (versionCode 7) – TeXslate-Rebranding-Release; Changelogs en/de

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -26,8 +26,8 @@ android {
         applicationId = "de.bgg_home.texslate"
         minSdk = 26
         targetSdk = 36
-        versionCode = 6
-        versionName = "1.0-alpha6"
+        versionCode = 7
+        versionName = "1.0-alpha7"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
