eed0b18cb4b1e56017583f31863b46b14017d48f
thobgg <thomas@bgg-mail.de>
2026-07-13 11:43:03 +0200

    Release: 1.0-alpha10 (versionCode 10) – flüssiges Markieren (Sidebar-Gesten-Fix); Changelogs en/de

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -26,8 +26,8 @@ android {
         applicationId = "de.bgg_home.texslate"
         minSdk = 26
         targetSdk = 36
-        versionCode = 9
-        versionName = "1.0-alpha9"
+        versionCode = 10
+        versionName = "1.0-alpha10"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
