527992c41300aa687bd5507fdd64314129342989
thobgg <thomas@bgg-mail.de>
2026-08-01 12:00:38 +0200

    Release: 1.0-alpha18 (versionCode 18) – Leer-Dokument-Meldung + sofortige Speichern-Bestätigung

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -26,8 +26,8 @@ android {
         applicationId = "de.bgg_home.texslate"
         minSdk = 26
         targetSdk = 36
-        versionCode = 17
-        versionName = "1.0-alpha17"
+        versionCode = 18
+        versionName = "1.0-alpha18"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
