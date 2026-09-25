7eb6500f87a57699cc2927e3053d48f3194959f3
thobgg <thomas@bgg-mail.de>
2026-09-25 15:49:36 +0200

    Release: 1.0-alpha25 (versionCode 25) – Touchpad-Scrollen und Tastenkuerzel (#8)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -35,8 +35,8 @@ android {
         applicationId = "de.bgg_home.texslate"
         minSdk = 26
         targetSdk = 36
-        versionCode = 24
-        versionName = "1.0-alpha24"
+        versionCode = 25
+        versionName = "1.0-alpha25"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
